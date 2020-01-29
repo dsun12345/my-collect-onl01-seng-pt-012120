9568a33657f8b469d8bfc48bd25a7bd60c8c585e
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 13:52:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
-  collection = []
+  
   i = 0 
+  collection = []
   while i < array.length 
-   collection << yield array[i]
+   collection << yield (array[i])
    i = i + 1 
  end 
  collection
